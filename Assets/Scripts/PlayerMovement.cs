9754a4b84342db0ca88f838fed6ee50ba2f903bc
ipop1 <ioan.pop@live.com>
2020-07-14 17:44:32 -0400

    Implemented player movement using WASD keys

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float movementSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        characterController.Move(direction * movementSpeed * Time.deltaTime);
+    }
+}
