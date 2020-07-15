0efa84d974cb840bfaa2dab396bb35537c14d3b0
ipop1 <ioan.pop@live.com>
2020-07-15 18:01:57 -0400

    Added rotation of the player relative to the direction of the camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] CharacterController characterController;
     [SerializeField] float movementSpeed = 5f;
+    [SerializeField] Transform followCamera;
+
+    float turnSmoothVelocity;
+    float turnSmoothTime = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +20,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        characterController.Move(direction * movementSpeed * Time.deltaTime);
+        if(direction.magnitude >= 0.1f) {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + followCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            characterController.Move(moveDirection * movementSpeed * Time.deltaTime);
+        }
     }
 }
